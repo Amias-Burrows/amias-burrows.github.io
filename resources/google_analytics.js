73acdf80dac20ad0ddd168f51c05e2f3bd26459f
Amias Burrows <amias.burrows@gmail.com>
2020-12-10 19:37:43 +0000

    UPDATE GOOGLE ANALYTICS TAG
    I have updated the Google analytics tag on the homepage.

--- resources/google_analytics.js
+++ resources/google_analytics.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 			function gtag(){dataLayer.push(arguments);}
 			gtag('js', new Date());
-			gtag('config', 'G-L4WGGKE73T');
+			gtag('config', 'UA-179156175-2');

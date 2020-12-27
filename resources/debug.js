2bf3a30732a51e13239ffd90bbdb7bfbd0c11e54
Amias Burrows <amias.burrows@gmail.com>
2020-12-27 09:49:43 +0000

    MORE CSS AND SOME MEDIA QUERIES
    I have added CSS and some media queries to make the website adapt to
    screen sizes.

--- resources/debug.js
+++ resources/debug.js
@@ -3,14 +3,10 @@ var debug = false;
 function debugPage() 
 {
 	if (debug) {
-		console.log("Turn Debug Off");
 		$('link[title=debug]')[0].disabled=true;
-		console.log("Works");
 		debug = false;
 	} else {
-		console.log("Turn Debug On");
 		$('link[title=debug]')[0].disabled=false;
-		console.log("Works");
 		debug = true;
 	};
 };
--- resources/image_choice.js
+++ resources/image_choice.js
@@ -1,22 +1,36 @@
-var[] chosenSlot = document.getElementByClassName('parallaxImage');
+var computer = document.createElement("img");
+computer.src = "resources/images/Code-min.jpg";
+computer.alt = "Photograph of computer";
 
-var image = Math.random() * (3 - 1) + 1;
+var recording = document.createElement("img");
+recording.src = "resources/images/Recording-studio-min.jpg";
+recording.alt = "Photograph of Recording Desk";
 
-for (i = 1, i < 4, i++)
+var typewriter = document.createElement("img");
+typewriter.src = "recources/images/Typewriter-min.jpg");
+typewriter.alt = "Photograph of typewriter";
+
+var[] image = {1, 2, 3};
+for (let i = image.length - 1; i > 0; i--)
 {
-	switch (image)
+	const j = Math.floor(Math.random() * i);
+	const temp = image[i];
+	image[i] = image[j];
+	image[j] = temp;
+};
+
+for (i = 0; i < 3; i++)
+{
+	switch (image(i))
 	{
 		case 1:
-			chosenSlot[i].src = "resources/images/Code-min.jpg";
-			chosenSlot[i].alt = "Photograph of computer";
+
 			break;
 		case 2:
-			chosenSlot[i].src = "resources/images/Recording-Studio-min.jpg";
-			chosenSlot[i].alt = "Photograph of Recording Desk";
+
 			break;
 		case 3:
-			chosenSlot[i].src = "resources/images/Typewriter-min.jpg";
-			chosenSlot[i].alt = "Photograph of Typewriter";
+
 			break;
 	};
 };
